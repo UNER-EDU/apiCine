65fa8d6a84287f65644b93420927efbbf931a6d3
cristianseltenreich <cristianseltenreich@gmail.com>
2023-11-11 13:49:00 -0300

    actualizacion paquetes y ruta
    actualizacion paquetes reinstalados venv y ruta correcta carpeta cristian

--- biblioteca.py
+++ biblioteca.py
@@ -13,7 +13,7 @@ from modelos.pelicula import Pelicula
 class Biblioteca:
     # Ruta de archivo Luis
     #__archivoDeDatos = "H:\\UNTDF UNER\\Programacion 2\\Trabajo Practico Final\\biblioteca.json"
-    __archivoDeDatos ="c:\\code\\apiCine\\biblioteca.json"
+    __archivoDeDatos ="c:\\code\\Api_cine_Uner\\\\apiCine\\biblioteca.json"    
     #__archivoDeDatos en ruta pc Cristian
     __actores = []
     __directores = []
